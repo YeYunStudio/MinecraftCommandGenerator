846e33c16e6de9ca75413153e0015d64bec6c6b6
雪浩 <34113239+james10949@users.noreply.github.com>
2020-04-02 14:15:15 +0800

    更新介面
    正在等待編輯器更新完成

---
+++ About.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MinecraftCommandGenerator
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.功能ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.工具ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關於ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟主選單ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.功能ToolStripMenuItem,
+            this.工具ToolStripMenuItem,
+            this.關於ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 功能ToolStripMenuItem
+            // 
+            this.功能ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟主選單ToolStripMenuItem});
+            this.功能ToolStripMenuItem.Name = "功能ToolStripMenuItem";
+            this.功能ToolStripMenuItem.Size = new System.Drawing.Size(53, 23);
+            this.功能ToolStripMenuItem.Text = "功能";
+            // 
+            // 工具ToolStripMenuItem
+            // 
+            this.工具ToolStripMenuItem.Name = "工具ToolStripMenuItem";
+            this.工具ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.工具ToolStripMenuItem.Text = "工具";
+            // 
+            // 關於ToolStripMenuItem
+            // 
+            this.關於ToolStripMenuItem.Name = "關於ToolStripMenuItem";
+            this.關於ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.關於ToolStripMenuItem.Text = "關於";
+            // 
+            // 開啟主選單ToolStripMenuItem
+            // 
+            this.開啟主選單ToolStripMenuItem.Name = "開啟主選單ToolStripMenuItem";
+            this.開啟主選單ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            this.開啟主選單ToolStripMenuItem.Text = "開啟主選單";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "About";
+            this.Text = "About";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 功能ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 工具ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關於ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟主選單ToolStripMenuItem;
+    }
+}
---
+++ main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MinecraftCommandGenerator
+{
+    partial class Minecraft指令生成器
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Minecraft指令生成器));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.main功能 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main開啟主選單 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main選項 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main分隔線 = new System.Windows.Forms.ToolStripSeparator();
+            this.main關閉 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main工具 = new System.Windows.Forms.ToolStripMenuItem();
+            this.main關於 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.main功能,
+            this.main工具,
+            this.main關於});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // main功能
+            // 
+            resources.ApplyResources(this.main功能, "main功能");
+            this.main功能.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.main開啟主選單,
+            this.main選項,
+            this.main分隔線,
+            this.main關閉});
+            this.main功能.Name = "main功能";
+            // 
+            // main開啟主選單
+            // 
+            resources.ApplyResources(this.main開啟主選單, "main開啟主選單");
+            this.main開啟主選單.Name = "main開啟主選單";
+            this.main開啟主選單.Click += new System.EventHandler(this.開啟主選單_Click);
+            // 
+            // main選項
+            // 
+            resources.ApplyResources(this.main選項, "main選項");
+            this.main選項.Name = "main選項";
+            this.main選項.Click += new System.EventHandler(this.選項_Click);
+            // 
+            // main分隔線
+            // 
+            resources.ApplyResources(this.main分隔線, "main分隔線");
+            this.main分隔線.Name = "main分隔線";
+            // 
+            // main關閉
+            // 
+            resources.ApplyResources(this.main關閉, "main關閉");
+            this.main關閉.Name = "main關閉";
+            this.main關閉.Click += new System.EventHandler(this.關閉_Click);
+            // 
+            // main工具
+            // 
+            resources.ApplyResources(this.main工具, "main工具");
+            this.main工具.Name = "main工具";
+            // 
+            // main關於
+            // 
+            resources.ApplyResources(this.main關於, "main關於");
+            this.main關於.Name = "main關於";
+            // 
+            // Minecraft指令生成器
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Minecraft指令生成器";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem main功能;
+        private System.Windows.Forms.ToolStripMenuItem main工具;
+        private System.Windows.Forms.ToolStripMenuItem main開啟主選單;
+        private System.Windows.Forms.ToolStripMenuItem main選項;
+        private System.Windows.Forms.ToolStripSeparator main分隔線;
+        private System.Windows.Forms.ToolStripMenuItem main關閉;
+        private System.Windows.Forms.ToolStripMenuItem main關於;
+    }
+}
+
